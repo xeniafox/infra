9e43eaddf82e8b98615a6a09ca3b00c3cfb37cdc
Lavínia Rodrigues <xenia@kernel32.xyz>
2024-08-18 08:57:39 -0300

    fix: add EF02 partition for generating boot entry

--- nodes/baphomet/disko-config.nix
+++ nodes/baphomet/disko-config.nix
@@ -9,6 +9,12 @@
           partitions = {
             boot = {
               name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+
+            esp = {
+              name = "ESP";
               size = "500M";
               type = "EF00";
               content = {
